6b4e3a33d6bef91f8987ee7f09c92f804c5f43bf
Hammad Kashmiri <154582006+HammadKashmiri1@users.noreply.github.com>
2025-07-06 07:48:45 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome,
+}
+
+document.getElementById("loginNow").onclick = function(){
+  window.location.href = "https://asquare04.github.io/carrybag-Ecommerce-Website/login.html";
+};
